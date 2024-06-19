bcacb917c69257ca083218b4e3cca24284f60e31
Daniel Silverio <danielsilverio81@outlook.com>
2024-06-19 15:04:34 -0300

    config project and folders and install views

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Head() {
+   return (
+    <>
+    <meta charSet="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </>
+   )
+}
