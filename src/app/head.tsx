a085b9d55ba4d318e0df4017945f8b7cb604c724
Daniel Silverio <danielsilverio81@outlook.com>
2024-06-19 15:43:10 -0300

    add folders components and services

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -5,6 +5,7 @@ export default function Head() {
     <>
     <meta charSet="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta property="og:title" content="EAD Netflix copy" key="title" />
     </>
    )
 }
