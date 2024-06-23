829295bb4f8d06cb9751761f11b451b00af3167c
Daniel Silverio <danielsilverio81@outlook.com>
2024-06-23 13:23:32 -0300

    changes in project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import styles from './homeNotAuth.module.scss'
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: 'Home',
@@ -7,9 +8,25 @@ export const metadata: Metadata = {
 
 const HomeNotAuth = () => {
   return (
-    <>
-    <main></main>
-    </>
+    <main>
+    <div className={styles.ctaSection}>
+        <Image
+        src="/homeNoAuth/logoCta.png"
+        width={30}
+        height={30}
+        alt="Picture of the autor"
+        className={styles.imgCta}
+        />
+        <p>Se Cadastre para ter acesso aos cursos</p>
+        <Image
+        src="/homeNoAuth/logoCta.png"
+        width={30}
+        height={30}
+        alt="Picture of the autor"
+        className={styles.imgCta}
+        />
+      </div>
+    </main>
   )
 }
 
