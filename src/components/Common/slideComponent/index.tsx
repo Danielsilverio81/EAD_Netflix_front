374f0a54f39a8a5af43cf022ebf0b86638d411ae
Daniel Silverio <danielsilverio81@outlook.com>
2024-07-31 19:53:46 -0300

    add backend

--- src/components/Common/slideComponent/index.tsx
+++ src/components/Common/slideComponent/index.tsx
@@ -1,17 +1,27 @@
+import { CourseType } from '@/services/courseService';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import  "@splidejs/splide/dist/css/splide.min.css"
+import SlideCard from '../slideCard';
 
-const SlideComponent = () => {
+interface props {
+    course: CourseType[];
+}
+
+const SlideComponent = ({course}: props) => {
     return (
     <>
     <div>
-        <Splide>
-            <SplideSlide options={{
+        <Splide options={{
                  type: "loop",
                  perPage: 4,
                  perMove: 1,
                  pagination:false,
-            }}></SplideSlide>
+            }}>
+            {course?.map((course)=> (
+                <SplideSlide key={course.id}>
+                    <SlideCard course={course}/>
+                </SplideSlide>
+            ))}
         </Splide>
     </div>
     </>
