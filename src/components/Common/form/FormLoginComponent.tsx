a1d579cac8d8b392b4c8294539823af7a1728ab7
Daniel Silverio <danielsilverio81@outlook.com>
2024-09-05 18:08:48 -0300

    add: Login page and toast for Login

--- src/components/Common/form/FormLoginComponent.tsx
+++ src/components/Common/form/FormLoginComponent.tsx
@@ -1,13 +1,54 @@
 "use client";
 import { Button, Container, Form, FormGroup, Input, Label } from "reactstrap";
 import styles from "./formStyles.module.scss";
+import { useRouter, useSearchParams } from "next/navigation";
+import { FormEvent, useEffect, useState } from "react";
+import ToastComponent from "../toast";
+import authService from "@/services/authService";
 
 const FormLoginComponent = () => {
+  const router = useRouter();
+  const [toastColor, setToastColor] = useState<string>("");
+  const [toastIsOpen, setToastIsOpen] = useState<boolean>(false);
+  const [toastMessage, setToastMessage] = useState<string>("");
+  const searchParams = useSearchParams();
+
+  const registerSuccess = searchParams.get("registred");
+  useEffect(() => {
+    if (registerSuccess === "true") {
+      setToastColor("bg-success");
+      setToastIsOpen(true);
+      setTimeout(() => {
+        setToastIsOpen(false);
+      }, 1000 + 5);
+      setToastMessage("Cadastro feito com Sucesso!");
+    }
+  }, [registerSuccess]);
+  const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const email = formData.get("email")!.toString();
+    const password = formData.get("password")!.toString();
+    const params = {email, password};
+
+    const {status} = await authService.login(params);
+
+    if (status === 200) {
+      router.push("/home")
+    } else {
+      setToastColor("bg-danger");
+      setToastIsOpen(true);
+      setTimeout(() => {
+        setToastIsOpen(false);
+      }, 1000 + 7);
+      setToastMessage("Erro de Email ou senha incorretos!");
+    }
+  }
   return (
     <>
       <Container className="py-5">
         <p className={styles.formTitle}>Bem-vindo(a) de Volta!</p>
-        <Form className={styles.form}>
+        <Form className={styles.form} onSubmit={handleLogin}>
           <p className="text-center">
             <strong>Bem-Vindo(a) ao OneBitFlix!</strong>
           </p>
@@ -37,10 +78,11 @@ const FormLoginComponent = () => {
               className={styles.input}
             />
           </FormGroup>
-          <Button outline className={styles.formBtn}>
+          <Button type="submit" outline className={styles.formBtn}>
             ENTRAR
           </Button>
         </Form>
+        <ToastComponent color={toastColor} isOpen={toastIsOpen} message={toastMessage}/>
       </Container>
     </>
   );
